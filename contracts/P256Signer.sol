07a95975f3915ace97155f80b61e03e15bba3a0d
oba <obatirou@gmail.com>
2023-10-04 12:27:33 +0200

    style: format P256 contracts

--- contracts/P256Signer.sol
+++ contracts/P256Signer.sol
@@ -8,55 +8,49 @@ import "solady/src/utils/Clone.sol";
 /// @dev This contract is the implementation. It is meant to be used through
 ///      proxy clone.
 contract P256Signer is Clone {
+    /// @notice The EIP-1271 magic value
+    bytes4 internal constant EIP1271_MAGICVALUE = 0x1626ba7e;
 
-  /// @notice The EIP-1271 magic value
-  bytes4 constant internal EIP1271_MAGICVALUE = 0x1626ba7e;
-  
-  /// @notice The old EIP-1271 magic value
-  bytes4 constant internal OLD_EIP1271_MAGICVALUE = 0x20c13b0b;
-  
-  /// @notice The x coordinate of the public key
-  /// @dev It uses the Clone helper library to get the x coordinate
-  ///      from calldata
-  function x() public pure returns (uint256) {
-    return _getArgUint256(0);
-  }
+    /// @notice The old EIP-1271 magic value
+    bytes4 internal constant OLD_EIP1271_MAGICVALUE = 0x20c13b0b;
 
-  /// @notice The y coordinate of the public key
-  /// @dev It uses the Clone helper library to get the y coordinate
-  ///      from calldata
-  function y() public pure returns (uint256) {
-    return _getArgUint256(32);
-  }
+    /// @notice The x coordinate of the public key
+    /// @dev It uses the Clone helper library to get the x coordinate
+    ///      from calldata
+    function x() public pure returns (uint256) {
+        return _getArgUint256(0);
+    }
 
-  /// @notice Error message when the signature is invalid
-  error InvalidSignature();
-  /// @notice Error message when the hash is invalid
-  error InvalidHash();
+    /// @notice The y coordinate of the public key
+    /// @dev It uses the Clone helper library to get the y coordinate
+    ///      from calldata
+    function y() public pure returns (uint256) {
+        return _getArgUint256(32);
+    }
 
-  function _validate(bytes memory data, bytes memory _signature) private view {
-    bytes32 _hash = keccak256(data);
-    (
-      bytes memory authenticatorData,
-      bytes memory clientData,
-      uint256 challengeOffset,
-      uint256[2] memory rs
-    ) = abi.decode(_signature, (bytes, bytes, uint256, uint256[2]));
+    /// @notice Error message when the signature is invalid
+    error InvalidSignature();
+    /// @notice Error message when the hash is invalid
+    error InvalidHash();
 
-    bool valid = Webauthn.checkSignature(
-      authenticatorData, 0x01, clientData, _hash, challengeOffset, rs, [x(), y()]
-    );
-    
-    if (!valid) revert InvalidSignature();
-  }
+    function _validate(bytes memory data, bytes memory _signature) private view {
+        bytes32 _hash = keccak256(data);
+        (bytes memory authenticatorData, bytes memory clientData, uint256 challengeOffset, uint256[2] memory rs) =
+            abi.decode(_signature, (bytes, bytes, uint256, uint256[2]));
 
-  function isValidSignature(bytes32 _hash, bytes memory _signature) public view returns (bytes4) {
-    _validate(abi.encode(_hash), _signature);
-    return EIP1271_MAGICVALUE;
-  }
+        bool valid =
+            Webauthn.checkSignature(authenticatorData, 0x01, clientData, _hash, challengeOffset, rs, [x(), y()]);
 
-  function isValidSignature(bytes memory _hash, bytes memory _signature) public view returns (bytes4) {
-    _validate(_hash, _signature);
-    return OLD_EIP1271_MAGICVALUE;
-  }
+        if (!valid) revert InvalidSignature();
+    }
+
+    function isValidSignature(bytes32 _hash, bytes memory _signature) public view returns (bytes4) {
+        _validate(abi.encode(_hash), _signature);
+        return EIP1271_MAGICVALUE;
+    }
+
+    function isValidSignature(bytes memory _hash, bytes memory _signature) public view returns (bytes4) {
+        _validate(_hash, _signature);
+        return OLD_EIP1271_MAGICVALUE;
+    }
 }
--- contracts/P256SignerFactory.sol
+++ contracts/P256SignerFactory.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.0;
 
 import "solady/src/utils/LibClone.sol";
@@ -6,24 +5,23 @@ import "solady/src/utils/LibClone.sol";
 /// @title P256SignerFactory
 /// @notice Factory contract for creating proxies for P256Signer
 contract P256SignerFactory {
+    /// @notice The implementation address of the P256Signer contract
+    address public immutable implementation;
 
-  /// @notice The implementation address of the P256Signer contract
-  address immutable public implementation; 
+    constructor(address implementation_) {
+        implementation = implementation_;
+    }
 
-  constructor(address implementation_) {
-    implementation = implementation_;
-  }
+    /// @notice Emitted when a new P256Signer proxy contract is created
+    event NewSignerCreated(uint256 indexed x, uint256 indexed y, address signer);
 
-  /// @notice Emitted when a new P256Signer proxy contract is created
-  event NewSignerCreated(uint256 indexed x, uint256 indexed y, address signer);
-
-  /// @notice Creates a new P256Signer proxy contract
-  /// @param x The x coordinate of the public key
-  /// @param y The y coordinate of the public key
-  function create(uint256 x, uint256 y) external {
-    bytes memory args = abi.encodePacked(x, y);
-    bytes32 salt = keccak256(args);
-    address signer = LibClone.cloneDeterministic(implementation, args, salt);
-    emit NewSignerCreated(x, y, signer);
-  }
+    /// @notice Creates a new P256Signer proxy contract
+    /// @param x The x coordinate of the public key
+    /// @param y The y coordinate of the public key
+    function create(uint256 x, uint256 y) external {
+        bytes memory args = abi.encodePacked(x, y);
+        bytes32 salt = keccak256(args);
+        address signer = LibClone.cloneDeterministic(implementation, args, salt);
+        emit NewSignerCreated(x, y, signer);
+    }
 }
