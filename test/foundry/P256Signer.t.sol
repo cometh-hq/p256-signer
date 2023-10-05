fc8674cb0904ac785cfcf565acff6033ae2dbaab
oba <obatirou@gmail.com>
2023-10-05 17:08:57 +0200

    test: add setUp TestP256Signer

--- test/foundry/P256Signer.t.sol
+++ test/foundry/P256Signer.t.sol
@@ -5,15 +5,19 @@ import {P256Signer} from "../../contracts/P256Signer.sol";
 import "forge-std/Test.sol";
 
 contract TestP256Signer is Test {
+    P256Signer signer;
+
+    function setUp() public {
+        signer = new P256Signer();
+    }
+
     function testDeploy() public {
-        P256Signer signer = new P256Signer();
         assertTrue(signer.initialized());
         assertEq(signer.x(), 0);
         assertEq(signer.y(), 0);
     }
 
     function testRevertInitializeImplementation() public {
-        P256Signer signer = new P256Signer();
         vm.expectRevert(P256Signer.AlreadyInitialized.selector);
         signer.initialize(1, 1);
     }
